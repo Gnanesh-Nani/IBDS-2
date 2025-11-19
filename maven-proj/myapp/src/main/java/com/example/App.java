a6064ece128ab7ce9d831bfb82806f46007e1500
Gnanesh-Nani <gnaneshgopinathan@example.com>
2025-11-19 21:56:44 +0530

    done

--- maven-proj/myapp/src/main/java/com/example/App.java
+++ maven-proj/myapp/src/main/java/com/example/App.java
@@ -1,13 +1,23 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.google.gson.Gson;
+
+public class App {
+    public static void main(String[] args) {
+        Gson gson = new Gson();
+        String json = gson.toJson(new Person("Ravi", 22));
+
+        System.out.println("JSON Output from Gson:");
+        System.out.println(json);
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 }
